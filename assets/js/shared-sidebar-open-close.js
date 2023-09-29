56755d39958621bca5afb722616d5aa61ee6cd7a
Abu Raihan <raihanahmed01973@gmail.com>
2023-09-29 18:15:15 +0600

    added pages box slider

--- assets/js/shared-sidebar-open-close.js
+++ assets/js/shared-sidebar-open-close.js
@@ -1,3 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var openBoxButton = document.getElementById("openBoxButton");
+  var pagesPreviewBox = document.querySelector(".pages-preview-box");
+
+  openBoxButton.addEventListener("click", function () {
+    // Toggle the visibility of the popup
+    if (pagesPreviewBox.style.display === "block") {
+      pagesPreviewBox.style.display = "none";
+    } else {
+      pagesPreviewBox.style.display = "block";
+    }
+  });
+
+  // Close the popup if the user clicks anywhere outside of it
+  document.addEventListener("click", function (event) {
+    if (
+      event.target !== openBoxButton &&
+      !openBoxButton.contains(event.target)
+    ) {
+      pagesPreviewBox.style.display = "none";
+    }
+  });
+});
+
+// setting sidebar open
+document.addEventListener("DOMContentLoaded", function () {
+  const openButton = document.getElementById("openSettingSidebarBtn");
+  const sidebar = document.getElementById("settingSidebar");
+  const overlay = document.getElementById("popSidebarOverlay");
+  const closeBtnPopSidebar = document.getElementById("closeBtnSettingSidebar");
+
+  openButton.addEventListener("click", function () {
+    sidebar.style.right = "0";
+    overlay.style.display = "block";
+    sidebar.style.display = "block";
+  });
+
+  overlay.addEventListener("click", function () {
+    sidebar.style.right = "-100%";
+    overlay.style.display = "none";
+  });
+
+  closeBtnPopSidebar.addEventListener("click", function () {
+    sidebar.style.right = "-100%";
+    overlay.style.display = "none";
+  });
+
+  function handleResize() {
+    if (window.innerWidth <= 586) {
+      sidebar.style.width = "100%";
+    } else {
+      sidebar.style.width = "586px";
+    }
+  }
+  handleResize();
+  window.addEventListener("resize", handleResize);
+});
+
 // compoenent 29 sidebar js
 document.addEventListener("DOMContentLoaded", function () {
   const openButton = document.getElementById("popSidebarSettingOpen");
